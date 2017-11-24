47776416dc192283e7ff32aeb877891080e524d8
Paul Spears <dpsthree@gmail.com>
2017-11-23 22:41:56 -0600

    build remote

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-yarn
+npm i -g @angular/cli rimraf copyfiles lerna
 npm run clean
 npm run bootstrap
 pushd ./packages/client
 npm run build
 popd
 pushd ./packages/server
-node strip-package.js
 popd
 npm run postbuild
